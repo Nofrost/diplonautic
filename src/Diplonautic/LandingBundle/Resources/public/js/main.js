61e5481bb5a17729e3a18f57188aa3aa98075c76
Dan <danmarc.rb@gmail.com>
2015-07-22 00:25:04 +0200

    res

--- src/Diplonautic/LandingBundle/Resources/public/js/main.js
+++ src/Diplonautic/LandingBundle/Resources/public/js/main.js
@@ -7,24 +7,28 @@ $(document).ready(function() {
     });
     //Carousel
     $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoWidht: true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:true,
-            loop:false
+        loop:true,
+        margin:10,
+        autoWidht: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:false
+            }
         }
-    }
-})
-;});
+    });
+    $( "#test_collapse_manual" ).click(function(e) {
+        console.log('aaa');
+    });
+
+});
