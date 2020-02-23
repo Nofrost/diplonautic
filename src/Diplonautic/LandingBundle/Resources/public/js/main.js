f321c2e1b908c64b916976b64cd205dc7c3b048a
DanMRB <danmarc.rb@gmail.com>
2020-02-23 23:47:56 +0100

    files minified and updated

--- src/Diplonautic/LandingBundle/Resources/public/js/main.js
+++ src/Diplonautic/LandingBundle/Resources/public/js/main.js
@@ -33,29 +33,93 @@ $(document).ready(function() {
     var imgWidth = $('.item.cap-bot img').width();
     $('.caption.simple-caption').css({width: imgWidth});
 
+    $('.footer__cookies').on('click', () => {
+        $(".modal").addClass("show");
+    }) 
+
     if($(".btn-cookie").length > 0){
         $( ".cookie-accept" ).click(function(event) {
             event.preventDefault();
-            cookieName = "cookiesAgreement";
-            cookieValue = "true";
-            document.cookie = cookieName +"=" + cookieValue;
+            acceptTCookies();
+            declineGCookies();
+            declineFCookies();
             $(".modal").removeClass("show");
             $(".modal").modal("hide");
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', 'UA-67738816-1', 'auto');
-            ga('send', 'pageview');
         });
-        $( ".cookie-decline" ).click(function(event) {
+        $( ".cookie-conf" ).click(function(event) {
             event.preventDefault();
-            cookieName = "cookiesAgreement";
-            cookieValue = "false";
-            document.cookie = cookieName +"=" + cookieValue;
+            $('.modal-content__info').addClass('hidden');
+            $('.modal-content__configuration').removeClass('hidden');
+        });
+        $( ".cookies__btn-back" ).click(function(event) {
+            $('.modal-content__info').removeClass('hidden');
+            $('.modal-content__configuration').addClass('hidden');
+        });
+        $('.cookies__btn-save').on('click', () => {
+            if ($('[name=techCookies]').is(":checked")) {
+                acceptTCookies();
+            } else {
+                declineTCookies();
+            }
+
+            if ($('[name=analCookies]').is(":checked")) {
+                acceptGCookies();
+            } else {
+                declineGCookies();
+                cookieName = "analyticsCookiesAgreement";
+                cookieValue = "false";
+                document.cookie = cookieName +"=" + cookieValue;
+            }
+
+            if ($('[name=funcCookies]').is(":checked")) {
+                acceptFCookies();
+            } else {
+                declineFCookies();
+            }
             $(".modal").removeClass("show");
             $(".modal").modal("hide");
         });
     }
-//    window.dispatchEvent(new Event('resize'));
 });
+
+const acceptTCookies = () => {
+    cookieName = "cookiesAgreement";
+    cookieValue = "true";
+    document.cookie = cookieName +"=" + cookieValue;
+}
+
+const declineTCookies = () => {
+    cookieName = "cookiesAgreement";
+    cookieValue = "false";
+    document.cookie = cookieName +"=" + cookieValue;
+} 
+
+const acceptGCookies = () => {
+    cookieName = "analyticsCookiesAgreement";
+    cookieValue = "true";
+    document.cookie = cookieName +"=" + cookieValue;
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-67738816-1', 'auto');
+    ga('send', 'pageview');
+}
+
+const declineGCookies = () => {
+    document.cookie = `_ga=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
+    document.cookie = `_gat=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
+    document.cookie = `_gid=; expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
+    window['ga-disable-UA-67738816-1'] = true;
+}
+
+
+const acceptFCookies = () => {
+    cookieName = "funcCookiesAgreement";
+    cookieValue = "true";
+    document.cookie = cookieName +"=" + cookieValue;
+}
+
+const declineFCookies = () => {
+    document.cookie = `funcCookiesAgreement=; expires = Thu, 01 Jan 1970 00:00:00 GMT`;
+}
